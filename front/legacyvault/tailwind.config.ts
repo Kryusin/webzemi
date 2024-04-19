5642f35af619f928f3561bf16fb4983f859d8347
LAPTOP-GBRNDHJ8\2210441 <2210441@ecc.ac.jp>
2024-04-19 10:20:50 +0900

    add: Text Components
    columns:
    role
    ・body(Default)
    ・title
    ・language
    ・errorTitle
    ・description
    ・delete
    ・edit
    ・errorDetails
    ・errorDescription
    children: ReactNode
    
    return ReactNode

--- front/legacyvault/tailwind.config.ts
+++ front/legacyvault/tailwind.config.ts
@@ -8,11 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'error-title-color': '#424B5A',
+        'description-color': '#A0A0A0',
+        'error-description-color': '#6B7280',
       },
+      fontSize: {
+        'body': '16px',
+        'title': '48px',
+        'language': '48px',
+        'error-title': '20px',
+        'description': '20px',
+        'delete': '16px',
+        'edit': '16px',
+        'error-details': '16px',
+        'error-description': '14px',
+      }
     },
   },
   plugins: [],
