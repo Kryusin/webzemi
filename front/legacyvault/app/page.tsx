3fa586d1bd29a7135e87acb0cfc4fb0eea8a1385
LAPTOP-GBRNDHJ8\2210441 <2210441@ecc.ac.jp>
2024-04-21 00:08:35 +0900

    add: create TextField component

--- front/legacyvault/app/page.tsx
+++ front/legacyvault/app/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import Text from "@/components/Text";
 import Logo from "@/components/logo/logo";
 import Sort from "@/components/Sort";
+import TextField from "@/components/Input/TextField";
 
 export default function Home() {
   return (
-    <div className="flex justify-end mr-2">
-      <Sort></Sort>
+    <div className="flex">
+      <TextField></TextField>
     </div>
   );
 }
---
+++ front/legacyvault/components/Input/TextField/index.tsx
@@ -0,0 +1,5 @@
+export default function TextField() {
+    return (
+        <input type="text" className="flex-[1_0_0] flex flex-row items-center h-[50px] border-2 outline-input focus:outline-input-hover rounded-lg pl-4 py-2" placeholder="エラー文を記述" />
+    )
+}
