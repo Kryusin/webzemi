eca913fb8bf130c88e941604dddc7a509ab74bec
LAPTOP-GBRNDHJ8\2210441 <2210441@ecc.ac.jp>
2024-04-21 11:46:47 +0900

    add: create TextArea component

---
+++ front/legacyvault/components/Input/TextArea/index.tsx
@@ -0,0 +1,5 @@
+export default function TextArea() {
+    return (
+        <textarea id="" cols={25} rows={10} className="flex-[1_0_0] rounded-lg bg-transparent outline-none border-2 border-input px-4 py-2 focus:border-input-hover resize-none" placeholder="エラーが起こる理由を記述"></textarea>
+    )
+}
--- front/legacyvault/components/Input/TextField/index.tsx
+++ front/legacyvault/components/Input/TextField/index.tsx
@@ -1,5 +1,5 @@
 export default function TextField() {
     return (
-        <input type="text" className="flex-[1_0_0] flex flex-row items-center h-[50px] border-2 outline-input focus:outline-input-hover rounded-lg pl-4 py-2" placeholder="エラー文を記述" />
+        <input type="text" className="flex-[1_0_0] flex flex-row items-center h-[50px] outline-2 outline-input focus:outline-input-hover rounded-lg pl-4 py-2" placeholder="エラー文を記述" />
     )
 }
