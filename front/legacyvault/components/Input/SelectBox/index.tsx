c6318fe296a46f4052b12e171d420ad98d7fb95e
LAPTOP-GBRNDHJ8\2210441 <2210441@ecc.ac.jp>
2024-04-21 23:22:26 +0900

    add: create code component

--- front/legacyvault/components/Input/SelectBox/index.tsx
+++ front/legacyvault/components/Input/SelectBox/index.tsx
@@ -1,41 +1,58 @@
 'use client'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useMonaco } from '@monaco-editor/react';
 
 import Text from '@/components/Text';
 import Image from 'next/image';
 
 export default function SelectBox({
-    language
+    nowlang,
+    onChange
 }: {
-    language: Array<string>
-}
-) {
+    nowlang: string,
+    onChange: (value: string) => void
+}) {
     const [Show, setShow] = useState<boolean>(false);
-    const [Select, setSelect] = useState<string>(language[0]);
+    const [Language, setLanguage] = useState<string[]>([]);
+    const monaco = useMonaco();
+
+    useEffect(() => {
+        console.log(monaco)
+        if (monaco) {
+            monaco.languages.getLanguages().map((language: any) => {
+                setLanguage((langs) => [...langs, language.id])
+            })
+        }
+    }, [monaco])
+
     return (
         <div className="flex justify-start">
-            <label htmlFor="sort" className="flex flex-row gap-[13px] px-4 py-2 bg-transparent rounded-lg border-2 border-input focus:border-input-hover" onClick={() => setShow(!Show)}>
-                <Text role="body">{Select}</Text>
+            <div className="flex flex-row gap-[13px] px-4 py-2 bg-transparent rounded-lg border-2 border-input focus:border-input-hover" onClick={() => setShow(!Show)}>
+                <Text role="body">{nowlang}</Text>
                 <Image src="/home/arrow.svg" alt="sort" width={16} height={16}></Image>
-            </label>
+            </div>
             {Show && (
-                <div className="absolute top-16 flex flex-col rounded-md bg-white p-1">
-                    {language.map((lang) => (
-                        <label
-                            htmlFor={lang}
-                            className="flex items-center gap-4 px-2 py-4 rounded-md hover:bg-sorted-hover"
-                            onClick={(e) => {
-                                setSelect(lang)
-                                setShow(false)
-                                console.log(lang)
-                            }}
-                            key={lang}
-                        >
-                            <span className={`${Select === lang ? 'bg-black' : 'bg-transparent'} w-2 h-2 rounded-full`}></span>
-                            <input type="radio" name="sort" value={lang} id={lang} className="hidden checked:after:bg-black focus:outline-none" />{lang}
-                        </label>
+                <div className="absolute top-16 flex flex-col rounded-md bg-white p-1 z-10 h-80 overflow-y-scroll drop-shadow-2xl custom-scroll">
+                    {Language.map((lang) => (
+                        <div key={lang}>
+                            <label
+                                htmlFor={lang}
+                                className="flex items-center gap-4 px-2 py-4 rounded-md hover:bg-sorted-hover"
+                            >
+                                <span className={`${nowlang === lang ? 'bg-black' : 'bg-transparent'} w-2 h-2 rounded-full`}></span>
+                                <span>{lang}</span>
+                            </label>
+                            <input
+                                type="radio"
+                                name="sort"
+                                value={lang}
+                                id={lang}
+                                className="hidden checked:after:bg-black focus:outline-none"
+                                onChange={(e) => onChange(e.target.value)}
+                                onClick={() => setShow(false)}
+                            />
+                        </div>
                     ))}
-
                 </div>
             )}
         </div>
