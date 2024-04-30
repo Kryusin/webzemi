e698118165d2b947b28303cab60c445a2e3b0463
hirai-asaki <a.hirai@union-eternity.co.jp>
2024-04-30 18:35:58 +0900

    add : edit page tranction

--- front/legacyvault/components/AddNote/index.tsx
+++ front/legacyvault/components/AddNote/index.tsx
@@ -4,29 +4,40 @@ import SelectBox from "@/components/Input/SelectBox"
 import TextArea from "@/components/Input/TextArea"
 import Code from "@/components/Input/Code"
 import Button from "@/components/Button"
-import { useState } from "react"
-import { InputProps } from "@/types"
+import { useEffect, useState } from "react"
+import { InputProps,SideBarProps } from "@/types"
 
-export default function AddNote() {
+export default function AddNote({status,data}: {status:string, data:InputProps}) {
     const [input, setInput] = useState<InputProps>({ id: 0, ErrorTitle: '', language: 'javascript', ErrorDetails: '', BeforeCode: '', ErrorReason: '', SolutionDetails: '', AfterCode: '', createdAt: ""});
+    useEffect(() => {
+        if(data.ErrorTitle.length > 0 && status === "edit") {
+            setInput(data)
+        }else{
+            setInput({id: 0, ErrorTitle: '', language: 'javascript', ErrorDetails: '', BeforeCode: '', ErrorReason: '', SolutionDetails: '', AfterCode: '', createdAt: ""})
+        }
+        console.log(data)
+    }, [data, status])
+    
     return (
         <>
-            <Text role="title">Add Note</Text>
+            <Text role="title">{status === "edit" ? "Edit Note" : "Add Note"}</Text>
             <div className="flex-[1_0_0] flex flex-col pl-5 gap-4">
                 <div className="justify-self-stretch flex flex-col gap-4">
                     <Text role="errorDetails">Error Title</Text>
                     <TextField
+                        value={input.ErrorTitle}
                         onChange={(value: string) => setInput({ ...input, ErrorTitle: value })}
                     ></TextField>
                 </div>
                 <div className="justify-self-stretch flex flex-col gap-4">
-                    <Text role="errorDetails">Error Title</Text>
-                    <SelectBox nowlang={input.language} onChange={(value: string) => setInput({ ...input, language: value })}></SelectBox>
+                    <Text role="errorDetails">Laguage</Text>
+                    <SelectBox value={input.language} onChange={(value: string) => setInput({ ...input, language: value })}></SelectBox>
                 </div>
                 <div className="justify-self-stretch flex flex-col gap-4">
                     <Text role="errorDetails">Error Details</Text>
                     <TextArea
                         state="detail"
+                        data={input.ErrorDetails}
                         onChange={(value: string) => setInput({ ...input, ErrorDetails: value })}
                     ></TextArea>
                 </div>
@@ -42,6 +53,7 @@ export default function AddNote() {
                     <Text role="errorDetails">Error Reason</Text>
                     <TextArea
                         state="reason"
+                        data={input.ErrorReason}
                         onChange={(value: string) => setInput({ ...input, ErrorReason: value })}
                     ></TextArea>
                 </div>
@@ -49,6 +61,7 @@ export default function AddNote() {
                     <Text role="errorDetails">Solution Details</Text>
                     <TextArea
                         state="solution"
+                        data={input.SolutionDetails}
                         onChange={(value: string) => setInput({ ...input, SolutionDetails: value })}
                     ></TextArea>
                 </div>
@@ -61,7 +74,7 @@ export default function AddNote() {
                     ></Code>
                 </div>
                 <div className="justify-self-stretch flex flex-row gap-4 justify-end">
-                    <Button role="add"></Button>
+                    {/* <Button role="edit"></Button> */}
                 </div>
             </div>
         </>
--- front/legacyvault/components/Input/TextField/index.tsx
+++ front/legacyvault/components/Input/TextField/index.tsx
@@ -1,10 +1,11 @@
-export default function TextField({ onChange }: { onChange: (valut: string) => void }) {
+export default function TextField({ value, onChange }: { value: string, onChange: (valut: string) => void }) {
     return (
         <input
             type="text"
             name="errorTitle"
             className="justify-self-stretch flex flex-row items-center h-[50px] outline-none border-2 border-solid border-input focus:border-input-hover rounded-lg pl-4 py-2"
             placeholder="エラー文を記述"
+            value={value}
             onChange={(e) => onChange(e.target.value)}
         />
     )
