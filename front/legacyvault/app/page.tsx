82dc21998eacc250a794be1583d9623426cea53c
hirai-asaki <a.hirai@union-eternity.co.jp>
2024-04-30 10:53:28 +0900

    add : page transition

--- front/legacyvault/app/page.tsx
+++ front/legacyvault/app/page.tsx
@@ -2,22 +2,30 @@
 import Sidebar from "@/components/Sidebar";
 import Home from "@/components/Home";
 import Setting from "@/components/Setting";
-import { PageProps } from "@/types";
+import { InputProps, SideBarProps } from "@/types";
+import AddNote from "@/components/AddNote";
+import Detail from "@/components/Detail";
 
 import { useState } from "react";
 
 export default function Page() {
-  const [open, setOpen] = useState<PageProps>(PageProps.Home);
+  const [open, setOpen] = useState<SideBarProps>(SideBarProps.Home);
+  const [errorData, setErrorData] = useState<InputProps>({id: 0, ErrorTitle: '', language: 'javascript', ErrorDetails: '', BeforeCode: '', ErrorReason: '', SolutionDetails: '', AfterCode: '', createdAt: ''});
   return (
     <div className="flex flex-row gap-0">
-      <Sidebar onClick={(value: PageProps) => setOpen(value)} />
+      <Sidebar onClick={(value: SideBarProps) => setOpen(value)} />
       <div className="flex-[4_0_0] flex flex-col gap-[38px] px-10 py-9 overflow-y-scroll h-screen">
-        {open === PageProps.Home ? (
-          <div>home</div>
-        ) : open === PageProps.AddNote ? (
-          <Home />
-        ) : open === PageProps.Setting && (
+        {open === SideBarProps.Home ? (
+          <Home onClick={(value: SideBarProps, data: InputProps) => {
+            setOpen(value)
+            setErrorData(data)
+          }}/>
+        ) : open === SideBarProps.AddNote ? (
+          <AddNote />
+        ) : open === SideBarProps.Setting ? (
           <Setting />
+        ) : open === SideBarProps.Detail && (
+          <Detail data={errorData}/>
         )}
       </div>
     </div>
