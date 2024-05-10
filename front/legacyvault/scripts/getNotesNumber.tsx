207db7d63ef5337cfcdcceb93dae59f0130835b5
hirai-asaki <a.hirai@union-eternity.co.jp>
2024-05-10 12:20:18 +0900

    add : Error text search

--- front/legacyvault/scripts/getNotesNumber.tsx
+++ front/legacyvault/scripts/getNotesNumber.tsx
@@ -1,4 +1,5 @@
 import { testData } from "@/components/testdata";
+import { InputProps } from "@/types"
 
 export const getNotesNumber = () => {
     const noteStatus = [...testData];
@@ -22,38 +23,42 @@ export const getNotesNumber = () => {
     return notesNumber;
 }
 
-export const allNotes = (sort:number) => {
-    const noteStatus = [...testData];
-    // 言語別に分ける
-    const language = noteStatus.map((note) => note.language);
-    const languageSet = new Set(language);
-    let languageArray = Array.from(languageSet);
-    const Number = languageArray.map((lang) => {
-        return {
-            language: lang,
-            num: noteStatus.filter((note) => note.language === lang).length
+export const allNotes = (testData:Array<InputProps>,sort:number) => {
+    if(testData) {
+        const noteStatus = [...testData];
+        
+        // 言語別に分ける
+        const language = noteStatus.map((note) => note.language);
+        const languageSet = new Set(language);
+        let languageArray = Array.from(languageSet);
+        const Number = languageArray.map((lang) => {
+            return {
+                language: lang,
+                num: noteStatus.filter((note) => note.language === lang).length
+            }
+        });
+        //昇順
+        if(sort == 0){
+            languageArray.sort()
+        //降順
+        }else if(sort == 1){
+            languageArray.sort().reverse()
+        //メモ数多い順
+        }else if(sort == 2){
+            Number.sort((a, b) => b.num - a.num);
+            languageArray = Number.map((data) => data.language )
+        //メモ数少ない順
+        }else if(sort == 3){
+            Number.sort((a, b) => b.num - a.num).reverse();
+            languageArray = Number.map((data) => data.language )
         }
-    });
-    //昇順
-    if(sort == 0){
-        languageArray.sort()
-    //降順
-    }else if(sort == 1){
-        languageArray.sort().reverse()
-    //メモ数多い順
-    }else if(sort == 2){
-        Number.sort((a, b) => b.num - a.num);
-        languageArray = Number.map((data) => data.language )
-    //メモ数少ない順
-    }else if(sort == 3){
-        Number.sort((a, b) => b.num - a.num).reverse();
-        languageArray = Number.map((data) => data.language )
+        const notesNumber = languageArray.map((lang) => {
+            return {
+                language: lang,
+                noteData: noteStatus.filter((note) => note.language === lang)
+            }
+        });
+        return notesNumber;
     }
-    const notesNumber = languageArray.map((lang) => {
-        return {
-            language: lang,
-            noteData: noteStatus.filter((note) => note.language === lang)
-        }
-    });
-    return notesNumber;
+    return [];
 }
