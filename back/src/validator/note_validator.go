97d5d0f166b71be5c3f5af40c98ab21d9a6c2f56
hirai-asaki <a.hirai@union-eternity.co.jp>
2024-05-24 09:52:47 +0900

    add notevalidata

--- back/src/validator/note_validator.go
+++ back/src/validator/note_validator.go
@@ -1,1 +1,58 @@
-package validator
+package validatorpackage validator
+
+import (
+	"sample.com/model"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+)
+
+type INoteValidator interface {
+	NoteValidate(note model.Note) error
+}
+
+type noteValidator struct{}
+
+func NewNoteValidator() INoteValidator {
+	return &noteValidator{}
+}
+
+func (nv *noteValidator) NoteValidate(note model.Note) error {
+	return validation.ValidateStruct(&note,
+		validation.Field(
+			&note.ErrorTitle,
+			validation.Required.Error("error_title is required"),
+			validation.RuneLength(1,).Error("Please enter at least one character"),
+		),
+		validation.Field(
+			&note.language,
+			validation.Required.Error("language is required"),
+			validation.RuneLength(1,).Error("Please enter at least one character"),
+		),
+		validation.Field(
+			&user.ErrorDetails,
+			validation.Required.Error("error_details is required"),
+			validation.RuneLength(1,).Error("Please enter at least one character"),
+		),
+		validation.Field(
+			&note.BeforeCode,
+			validation.Required.Error("beforeCode is required"),
+			validation.RuneLength(1,).Error("Please enter at least one character"),
+		),
+		validation.Field(
+			&note.ErrorReasonError,
+			validation.Required.Error("error_reason_error is required"),
+			validation.RuneLength(1,).Error("Please enter at least one character"),
+		),
+		validation.Field(
+			&note.SolutionDetails,
+			validation.Required.Error("solution_details is required"),
+			validation.RuneLength(1,).Error("Please enter at least one character"),
+		),
+		validation.Field(
+			&note.AfterCode,
+			validation.Required.Error("afterCode is required"),
+			validation.RuneLength(1,).Error("Please enter at least one character"),
+		),
+	)
+}
