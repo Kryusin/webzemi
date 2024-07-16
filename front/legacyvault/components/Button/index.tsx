e695a867eb33c781e42f065a5b2fd4b78d2221a2
平井旭晃 <a.hirai@union-eternity.co.jp>
2024-07-16 14:30:21 +0900

    web-36 delete

--- front/legacyvault/components/Button/index.tsx
+++ front/legacyvault/components/Button/index.tsx
@@ -10,7 +10,7 @@ export default function Button({
     const button = role === 'edit' ? (
         <button className="w-[150px] h-[40px] border-input bg-white rounded justify-center items-center font-bold hover:bg-[#f0f0f0]" type="submit" onClick={() => onClick(SideBarProps.AddNote, "edit")}>Edit</button>
     ) : role === 'delete' ? (
-        <button className="w-[150px] h-[40px] border-input bg-show-code rounded justify-center items-center text-white font-bold hover:bg-[#101927]" type="submit">Delete</button>
+        <button className="w-[150px] h-[40px] border-input bg-show-code rounded justify-center items-center text-white font-bold hover:bg-[#101927]" type="submit" onClick={onClick}>Delete</button>
     ) : role === 'save' ? (
         <button className="w-[150px] h-[40px] border-input bg-show-code rounded justify-center items-center text-white font-bold hover:bg-[#101927]" type="submit" onClick={onClick}>Save</button>
     ) : role === 'add' && <button className="w-[150px] h-[40px] border-input bg-show-code rounded justify-center items-center text-white font-bold hover:bg-[#101927]" type="submit" onClick={onClick}>Add</button>
